fd903da2c5963519215f125ab9c0d73243b11c78
Lewis Dean <lewisdean731@gmail.com>
2020-11-21 18:07:24 +0000

    feat: make log level configurable

--- bot.py
+++ bot.py
@@ -3,13 +3,15 @@ import discord
 from dotenv import load_dotenv
 import logging
 
-logging.basicConfig(level=logging.INFO)
-
 # Provide .env file with following environment variables:
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+LOGLEVEL = str(os.getenv('LOGLEVEL'))
 CHANNELS = str(os.getenv('MONITORED_CHANNELS')).split(",") 
+
+# https://docs.python.org/3/library/logging.html#logging-levels
+logging.basicConfig(level=LOGLEVEL)
 
 client = discord.Client()
 
